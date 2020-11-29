f4f33b6d699a575e983c8c8165dc7bc1663fac12
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-11-29 23:08:24 +0200

    add normalayz

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import 'modern-normalize/modern-normalize.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root'));
